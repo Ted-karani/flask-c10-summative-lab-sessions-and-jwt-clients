51da897735ea974a3f59a6d31f8858f19ad4b279
Ted karani <ted.karani@student.moringaschool.com>
2026-08-02 21:56:26 +0300

    add signup route with password confirmation check

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request, session
 from flask_migrate import Migrate
-from flask_restful import Api
+from sqlalchemy.exc import IntegrityError
 
-from models import db, bcrypt
+from models import db, bcrypt, User, Note
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -15,9 +15,34 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 bcrypt.init_app(app)
-api = Api(app)
 
-# Routes go here - we'll add these next
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+
+    username = data.get('username')
+    password = data.get('password')
+    password_confirmation = data.get('password_confirmation')
+
+    if password != password_confirmation:
+        return {'errors': ['Passwords do not match']}, 422
+
+    try:
+        user = User(username=username)
+        user.password_hash = password
+
+        db.session.add(user)
+        db.session.commit()
+
+        session['user_id'] = user.id
+
+        return {'id': user.id, 'username': user.username}, 201
+
+    except IntegrityError:
+        db.session.rollback()
+        return {'errors': ['Username already taken']}, 422
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
